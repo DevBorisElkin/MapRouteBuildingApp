0519a6e96dbfb4da55948b963a7ba15f8e088655
Boris <ea.foreverrr@gmail.com>
2022-05-15 22:46:19 +0300

    Add Apple map to the view

---
+++ MapRouteBuildingApp/TaskDescription.swift
@@ -0,0 +1,17 @@
+//
+//  TaskDescription.swift
+//  MapRouteBuildingApp
+//
+//  Created by test on 15.05.2022.
+//
+
+import Foundation
+
+/*
+ 
+ Задача
+ 
+ Реализовать приложение для построение кратчайшего маршрута через заданные географические точки
+ 
+ 
+ */
