69e6919f98f418c5ff3309d73cbac67bbb3f8c31
Boris <ea.foreverrr@gmail.com>
2022-05-15 22:23:20 +0300

    Initial commit

---
+++ MapRouteBuildingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapRouteBuildingApp
+//
+//  Created by test on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
