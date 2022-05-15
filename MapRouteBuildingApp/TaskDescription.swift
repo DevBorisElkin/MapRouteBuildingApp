91aa39b550167c432a3850cb71a71426a66e14b1
Boris <ea.foreverrr@gmail.com>
2022-05-15 23:38:11 +0300

    backup links

--- MapRouteBuildingApp/TaskDescription.swift
+++ MapRouteBuildingApp/TaskDescription.swift
@@ -13,5 +13,8 @@ import Foundation
  
  Реализовать приложение для построение кратчайшего маршрута через заданные географические точки
  
- 
+ Apple documentation:
+ https://developer.apple.com/documentation/corelocation/adding_location_services_to_your_app
+ https://developer.apple.com/documentation/mapkit/#topics
+ https://developer.apple.com/documentation/mapkit/mkmapview
  */
