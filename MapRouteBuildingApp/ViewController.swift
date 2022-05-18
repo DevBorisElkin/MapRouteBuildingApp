e1fc2b147be04b39d2f2ac406b225b0ab7c47907
Boris <ea.foreverrr@gmail.com>
2022-05-18 08:23:06 +0300

    Added simple buttons

--- MapRouteBuildingApp/ViewController.swift
+++ MapRouteBuildingApp/ViewController.swift
@@ -17,11 +17,52 @@ class ViewController: UIViewController {
         return mapView
     }()
     
+    let addAddressButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "button_address"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let routeButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "button_route"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let resetButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "button_reset"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setConstraints()
+        
+        //print(testClassVariable.number)
+        //testClassVariable = TestClass()
+        
+        addAddressButton.addTarget(self, action: #selector(addAddressButtonTapped), for: .touchUpInside)
+        routeButton.addTarget(self, action: #selector(routeButtonTapped), for: .touchUpInside)
+        resetButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
     }
 
+    @objc func addAddressButtonTapped(){
+        print("TapAdd")
+    }
+    
+    @objc func routeButtonTapped(){
+        print("TapRoute")
+    }
+    
+    @objc func resetButtonTapped(){
+        print("TapReset")
+    }
+    
 
 }
 
@@ -35,7 +76,48 @@ extension ViewController {
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
         ])
+        
+        mapView.addSubview(addAddressButton)
+        NSLayoutConstraint.activate([
+            addAddressButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 50),
+            addAddressButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            addAddressButton.heightAnchor.constraint(equalToConstant: 55),
+            addAddressButton.widthAnchor.constraint(equalToConstant: 140)
+        ])
+        
+        mapView.addSubview(routeButton)
+        NSLayoutConstraint.activate([
+            routeButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20),
+            routeButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            routeButton.heightAnchor.constraint(equalToConstant: 55),
+            routeButton.widthAnchor.constraint(equalToConstant: 140)
+        ])
+
+        mapView.addSubview(resetButton)
+        NSLayoutConstraint.activate([
+            resetButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            resetButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            resetButton.heightAnchor.constraint(equalToConstant: 55),
+            resetButton.widthAnchor.constraint(equalToConstant: 140)
+        ])
     }
     
 }
+
+//var testClassVariable : TestClass{
+//    print("1) Created variable of a test class")
+//    var variable:TestClass = TestClass()
+//    variable.number = 2
+//    return variable
+//}
+//
+//class TestClass{
+//    var number:Int = 0
+//
+//    init(){
+//        print("2) called initializer of TestClass")
+//    }
+//}
+
+
 
